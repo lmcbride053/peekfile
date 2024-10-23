7b9e99d329512bb7c1d02f94d5e7f0f60f06600c
Liam McBride <liammcbride@mar-102-151.eduroamcm.upf.edu>
2024-10-23 16:26:29 +0200

    second commit

--- peek.sh
+++ peek.sh
@@ -1,7 +1,7 @@
 ### Script to print the first and last n lines of a file
 
-head -n 3 ../dataset1/tables/target_prokaryotes.txt
+head -n $2 $1
 
 echo ...
 
-tail -n 3 ../dataset1/tables/target_prokaryotes.txt
+tail -n $2 $1

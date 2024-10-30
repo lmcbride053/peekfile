d426f95ca599b94d3f4041a37ceb3365bbf08f06
Liam McBride <liammcbride@Liams-MacBook-Air.local>
2024-10-30 22:04:25 +0100

    Peek Updates for Oct 31

--- peek.sh
+++ peek.sh
@@ -1,7 +1,21 @@
 ### Script to print the first and last n lines of a file
 
-head -n $2 $1
+lines="$2"
 
-echo ...
 
-tail -n $2 $1
+# default number of lines is 3
+if [[ -z "$lines" ]]; then
+	lines="3"
+fi
+
+# check the 2X
+line_count=$(wc -l < "$1")
+if (( line_count <= 2 * $lines )); then
+	cat "$1"
+else
+  echo "WARNING, the file is really big tbh, only displaying the first and last $lines lines"
+  head -n "$lines" "$1"
+  echo ...
+  tail -n "$lines" "$1"
+fi
+

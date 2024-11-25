12ed1792e9ed90092217ff214376b52079a26f83
Liam McBride <liammcbride@Liams-MacBook-Air.local>
2024-11-25 13:51:05 +0100

    adding initial script to github

---
+++ fastascan.sh
@@ -0,0 +1,15 @@
+#### FASTASCAN SCRIPT ####
+
+#### ALG MIDTERM 3 ####
+
+# Last edited Mon Nov 25
+
+
+# Identifying the arguments (directory and number of lines)
+
+directory="$1"
+
+lines="$2"
+
+echo $1 is the directory we are searching in and $2 is the number of lines we will be using for displaying content
+

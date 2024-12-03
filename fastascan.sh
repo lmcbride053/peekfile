c11c96a4d9acbd00c54c89477f8328060a015025
Liam McBride <liammcbride@Liams-MacBook-Air.local>
2024-12-03 12:26:08 +0100

    added the extra points condition and some exit statements. almost done!

--- fastascan.sh
+++ fastascan.sh
@@ -11,6 +11,7 @@ directory="$1"
 
 lines="$2"
 
+
 # default directory is current dir
 if [[ -z "$directory" ]]; then
 	directory="."
@@ -21,18 +22,45 @@ if [[ -z "$lines" ]]; then
 	lines="0"
 fi
 
+
+# Check if directory is real and is a directory
+if [[ ! -d "$directory" ]]; then
+	echo $directory does not exist or is not a directory, try a different one or maybe check your spelling 
+	exit 1
+fi
+
+# Check if there are fasta files in directory
+numfafiles=$(find $directory -name "*.fa" -or -name "*.fasta" | wc -l)
+if [[ $numfafiles -eq 0 ]]; then
+	echo There are no fasta files in this folder to examine. Try a different folder 
+	exit 1
+fi
+
+
+
+
 ############################################## Welcome message ##########################################
 
-echo Hello bash user! Welcome to $directory!
+echo Hello bash user!
 echo
-echo We are just going to be taking a quick look around at the fasta files in this folder with help from your little friend fastascan.sh.
+echo We are just going to be taking a quick look around at the fasta files in the folder you chose with help from your little friend fastascan.sh.
+echo 
+echo
+echo For reference, the standard syntax for running this script is:
+echo
+echo $ bash /path/to/fastascan.sh directory numlines
+echo
+echo If you do not enter directory or line inputs, the default will be current folder and 0 lines
+echo
 echo
 echo Lets get started!
+echo ------------------------------------------------------------------------
+
 echo
+
 
 ########### Identifying num fasta files and unique ids in 'directory'  ##############
 
-numfafiles=$(find $directory -name "*.fa" -or -name "*.fasta" | wc -l)
 
 echo It looks like there are $numfafiles fasta files in this directory
 echo
@@ -60,10 +88,14 @@ find $directory -name "*.fa" -or -name "*.fasta" | while read i; do
   	fi
   	echo The total sequence length of this file is: $(echo $(grep ">" -v $i) | awk '!/>/{gsub(/-/, "", $0); print $0}' | awk '!/>/{gsub(/ /, "", $0); print $0}' | wc -c)
   	echo
-  	echo sneak peak at contents:
+  	echo $(grep ">" -v $i) | awk '!/>/{gsub(/-/, "", $0); print $0}' | awk '!/>/{gsub(/ /, "", $0); print $0}' > seq.txt
+  	if [[ -z $(grep [B,D-F,H-M,O-S,U-Z] seq.txt) ]]; then echo NUCLEOTIDE SEQ; else echo PROTEIN SEQ; fi
+  	echo
   	echo
     line_count=$(wc -l < "$i")
-  	if (( $line_count <= 2 * $lines )); then
+    if (( $lines == 0 )); then
+    	echo electing to not sneak peak at file contents
+  	elif (( $line_count <= 2 * $lines )); then
 		cat "$i"
 	else
   		head -n "$lines" "$i"
